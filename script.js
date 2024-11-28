7c6724ea017c4505743674c3d571655ac8aa630b
bsp3725k <139534037+bsp3725k@users.noreply.github.com>
2024-11-28 18:34:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 function storage() {
 	return (`First Name: ${document.getElementById("firstName").value} \n Last Name: ${document.getElementById("lastName").value} \n Phone Number: ${document.getElementById("phoneNumber").value} \n Email ID: ${document.getElementById("email").value}`);
 }
-let btn = document.getElementById("submit");
+let btn = document.getElementById("sub");
 btn.addEventListener("click",()=> {
 	alert(storage());
 })
