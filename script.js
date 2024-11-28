882da9e11c9b8f7f65b0a7795a6b610998fd918a
bsp3725k <139534037+bsp3725k@users.noreply.github.com>
2024-11-28 18:26:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function storage() {
+	return (`First Name: ${document.getElementById("firstName").value} Last Name: ${document.getElementById("lastName").value} Phone Number: ${document.getElementById("phoneNumber").value} Email ID: ${document.getElementById("email").value}`);
+}
+let btn = document.getElementById("submit");
+btn.addEventListener("click",()=> {
+	alert(storage());
+})
